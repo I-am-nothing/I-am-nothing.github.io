78b755ad621fdf357f66ebc022c24db8599a03e8
sakun921118 <jdps99119@gmail.com>
2022-06-24 03:42:41 +0800

    Update GA

--- dev/.vuepress/config.js
+++ dev/.vuepress/config.js
@@ -1,20 +1,4 @@
 module.exports = {
-  head: [
-    [
-        'script',
-        {
-            async: true,
-            src: 'https://www.googletagmanager.com/gtag/js?id=G-170VJJB3ZL',
-        },
-    ],
-    [
-        'script',
-        {},
-        [
-            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-170VJJB3ZL');",
-        ],
-    ],
-],
   locales: {
     '/': {
       lang: 'en-US',
