4731722a795a9b82267ccbec4b85688ca3f1dc6b
sakun921118 <jdps99119@gmail.com>
2022-06-30 03:21:31 +0800

    Fix Image Issue

--- dev/.vuepress/theme/components/HomeBlog/index.vue
+++ dev/.vuepress/theme/components/HomeBlog/index.vue
@@ -139,7 +139,7 @@ export default defineComponent({
   margin: 0px auto;
   background-image: url('../../images/background.jpg')
   background-repeat: no-repeat
-  background-size: cover
+  background-size: auto 1000px;
   background-attachment: fixed
   background-position: center
   .hero {
